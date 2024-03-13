17fdab9fca1147315edff8178d221c80364c4ee9
Krishna Reddy <vkreddy177@gmail.com>
2024-03-13 07:48:27 +0530

    upd

--- src/main/java/com/vkr/springbootjenkinsdockerk8s/SpringBootJenkinsDockerK8sApplication.java
+++ src/main/java/com/vkr/springbootjenkinsdockerk8s/SpringBootJenkinsDockerK8sApplication.java
@@ -2,7 +2,6 @@ package com.vkr.springbootjenkinsdockerk8s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootJenkinsDockerK8sApplication {
